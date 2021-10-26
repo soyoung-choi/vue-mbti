59a52c45eeede5cdcd093134dc8e1a085c6b66e2
Soyoung <sydesign24@naver.com>
2021-10-26 20:26:30 +0900

    css 연결

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import "@/assets/css/common.css";
+import "@/assets/css/element-ui.css";
+import "@/assets/css/button.css";
+import "@/assets/css/color.css";
+import "@/assets/css/text.css";
+import "@/assets/css/mobile.css";
+import "@/assets/css/distance.css";
+import "@/assets/css/badge.css";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {};
+</script>
